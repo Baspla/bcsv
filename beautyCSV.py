25a1450c19ce40dfa747c8be8e57b87f82577a6c
Tim <timmorgner@gmail.com>
2021-07-07 18:15:53 +0200

    Initial commit

---
+++ beautyCSV.py
@@ -0,0 +1,28 @@
+#Grüße von Tim
+
+import csv
+import sys
+import datetime
+
+def process():
+    if len(sys.argv) < 3:
+        print("Fehlender Datei Parameter")
+        return
+    f = open(sys.argv[2], "w")
+    with open(sys.argv[1]) as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for row in reader:
+            if len(row)==4:
+                if row[3]!="":
+                    row.pop(0)
+                    row.pop(1)
+                    f.write(";".join(row)+"\n")
+                    print("["+datetime.datetime.now().strftime("%H:%M:%S")+"] Zeile: "+str(reader.line_num))
+                else:
+                    print("["+datetime.datetime.now().strftime("%H:%M:%S")+"] Zeile: "+str(reader.line_num)+" ist leer")
+    f.close()
+    print("#TimLoben")
+
+if __name__ == '__main__':
+    process()
+
